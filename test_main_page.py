9bf221eae77094b444307ea34ec4540959ef13f2
VictoryaMetelica <viv.kutas@gmail.com>
2024-06-04 11:36:38 +0300

    Выполнили неявный переход на страницу.

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ import time
 
 import PageObject_Selenium_Python.pages.locators
 from PageObject_Selenium_Python.pages.main_page import MainPage
-
+from PageObject_Selenium_Python.pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -10,11 +10,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
         # открываем страницу
     page.open()
-        # выполняем метод страницы - переходим на страницу логина
-    login_page = page.go_to_login_page()
+        # выполняем метод страницы - неявно переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page(browser)
     time.sleep(2)
-
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
