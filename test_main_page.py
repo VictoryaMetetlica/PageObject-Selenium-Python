f717e44ad24530fb4d6671f5276d97c793261cef
VictoryaMetelica <viv.kutas@gmail.com>
2024-06-12 12:20:28 +0300

    Добавили группировку @pytest.mark.login_guest в в класс class TestLoginFromMainPage() двух методов.

--- test_main_page.py
+++ test_main_page.py
@@ -3,23 +3,25 @@ from PageObject_Selenium_Python.pages.main_page import MainPage
 from PageObject_Selenium_Python.pages.login_page import LoginPage
 from PageObject_Selenium_Python.pages.product_page import ProductPage
 from PageObject_Selenium_Python.pages.basket_page import BasketPage
+import pytest
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-        # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser, link)
-        # открываем страницу
-    page.open()
-        # выполняем метод страницы - неявно переходим на страницу логина
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page(browser)
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+            # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = MainPage(browser, link)
+            # открываем страницу
+        page.open()
+            # выполняем метод страницы - неявно переходим на страницу логина
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page(browser)
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -29,7 +31,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     time.sleep(2)
     page.basket_is_empty(browser)
     page.should_not_be_product_in_basket()
-
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
